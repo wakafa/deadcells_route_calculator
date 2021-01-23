0b1537adf47b6c9e21b6c78beb424b8f63beeb4b
morco <mor.cohen1989@gmail.com>
2021-01-23 21:24:04 +0200

    get biomes list; first biome in route

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const express = require('express')
 const { spawn } = require('child_process');
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 3000
-const path = require('path')
 
-let biomesList;
+const BIOMES_DIR = "Biomes"
 const python = spawn('python', [path.join('data_gathering', 'main.py')]);
 
-python.stdout.on('data', data => {
-    console.log('Pipe data from python script ...');
-    biomesList = data.toString();
-    console.log("Biomes list updated")
-});
+this.biomesList = {
+    biomes: []
+}
+
+let get_biome_list = () => {
+    fs.readdir(BIOMES_DIR, (err, files) => {
+        files.forEach(file => {
+            let data = fs.readFileSync(path.join(BIOMES_DIR, file))
+            biomeData = JSON.parse(data)
+            if (biomeData.unused) {
+                console.log(`Skipping unused map : ${biomeData.name}`)
+            } else {
+                this.biomesList.biomes.push(biomeData)
+            }
+        })
+    })
+}
+
 python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
+    console.log(`Done collecting data, code :  ${code}`);
+    biomesList = get_biome_list()
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'welcome.html'))
 })
 
-
 app.get('/biomes_list', (req, res) => {
-    res.send(biomesList)
+    res.send(this.biomesList)
 })
 
-
 app.use(express.static(path.join(__dirname, 'scripts')))
+    // app.use(express.static(path.join(__dirname)))
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,3 +1,60 @@
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
 window.onload = () => {
     console.log("Welcome!")
+}
+
+let showSpoilers = false
+let bcs = 0
+let biomesList = JSON.parse(httpGet('/biomes_list')).biomes
+let currentBiome
+let currentRoute = []
+
+let toggleSpoilers = () => {
+    var spoilersButton = document.getElementById("spoilers-button")
+    if (!showSpoilers) {
+        spoilersButton.textContent = "Spoilers warning!"
+        showSpoilers = true
+    } else {
+        spoilersButton.textContent = "Spoilers free"
+        showSpoilers = false
+    }
+}
+
+let setBcs = () => {
+    bcsElem = document.getElementById("bcs")
+    bcs = bcsElem.value
+}
+
+let get_entry_biome = () => {
+    return biomesList.filter(b => b.data[0].entrances.length === 0)[0]
+}
+
+let display_route = () => {
+    let currBiomeData = currentRoute[0].data
+    console.log("XD")
+    console.log(currentBiome.name)
+    console.log(currentBiome.data)
+    let spoiles = {
+        scrolls: {
+            power: currentBiome.scrolls.power,
+            dual: currentBiome.scrolls.dual
+        }
+    }
+    console.log(currBiomeData)
+        // currentRoute.forEach(route => {
+        //     console.log(route.data[bcs])
+        // })
+}
+
+let start_route = () => {
+    currentBiome = get_entry_biome()
+    document.getElementById("start-route").textContent = currentBiome.name
+    currentRoute = [currentBiome]
+    display_route()
 }
